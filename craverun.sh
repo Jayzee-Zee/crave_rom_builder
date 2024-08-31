1308b392c04e4489e242c66de2ab4dc7ecf0fccf
Jayzee-Zee <Jayzee-Zee@users.noreply.github.com>
2024-08-31 12:26:11 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 rm -rf crave_rom_builder
-git clone https://$GH_TOKEN@github.com/xc112lg/crave_rom_builder.git
+git clone https://$GH_TOKEN@github.com/Jayzee-Zee/crave_rom_builder.git
 cp -f crave_rom_builder/pull.sh pull.sh
 chmod +x pull.sh
 crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
-
-rm -rf .repo/local_manifests 
-git clone https://github.com/xc112lg/local_manifests -b main .repo/local_manifests
-/opt/crave/resync.sh
- 
-source build/envsetup.sh
-lunch lineage_h872-userdebug
-m installclean
-m bacon
+curl https://raw.githubusercontent.com/Jayzee-Zee/build_scripts/AOSPA/build.sh | bash
 "
 . pull.sh
